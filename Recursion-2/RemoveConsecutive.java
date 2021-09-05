938399154fa77ddc4ad9f6c4835ce0ed2cc60ae9
Gajraj Mohini <mohinigajraj145@gmail.com>
2021-09-05 07:13:35 +0530

    remove consecutive commit

---
+++ Recursion-2/RemoveConsecutive.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class RemoveConsecutive {
+	public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+    	String input = s.next();
+    	System.out.println(removeConsecutiveDuplicates(input));
+    }
+    public static String removeConsecutiveDuplicates(String s) {
+		
+	}
+}
