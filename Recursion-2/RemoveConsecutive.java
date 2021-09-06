08188d90ccdb233358deb6e03ae62feae0bf953a
Gajraj Mohini <mohinigajraj145@gmail.com>
2021-09-06 05:44:29 +0530

    remove duplicates

--- Recursion-2/RemoveConsecutive.java
+++ Recursion-2/RemoveConsecutive.java
@@ -7,6 +7,8 @@ public class RemoveConsecutive {
     	System.out.println(removeConsecutiveDuplicates(input));
     }
     public static String removeConsecutiveDuplicates(String s) {
-		
+		if(s.length() ==0 || s.length()==1) return s;
+        if(s.charAt(0) == s.charAt(1)) return removeConsecutiveDuplicates(s.substring(1));
+        else return s.charAt(0) + removeConsecutiveDuplicates(s.substring(1));
 	}
 }
