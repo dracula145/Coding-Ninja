a99158fdedd4e215630dfca4bdde9f2a5d52d4b7
Gajraj Mohini <mohinigajraj145@gmail.com>
2021-09-06 07:00:00 +0530

    merge sort done

--- Recursion-2/MergeSort.java
+++ Recursion-2/MergeSort.java
@@ -23,14 +23,17 @@ public class MergeSort {
 		printArray(input);
 	}
     public static void mergeSort(int[] input){
-		myMergeSort(input, 0, input.length-1);
+		sort(input, 0, input.length-1);
 	}
-    public static void myMergeSort(int [] input, int l, int r){
+    public static void sort(int [] input, int l, int r){
         if(l<r){
             int mid = (l+r)/2;
-            myMergeSort(input, l, mid);
-            myMergeSort(input, mid+1, r);
+            sort(input, l, mid);
+            sort(input, mid+1, r);
             merge(input, l, mid, r);
+            System.out.print("merged - ");
+            printArray(input);
+            System.out.println();
         }
     }
 
@@ -44,7 +47,7 @@ public class MergeSort {
         if(i>mid) while(j<=r) b[k++] = input[j++];
         else while(i<=mid) b[k++] = input[i++];
 
-        for(i=0;i<=r;i++) input[i] = b[i];
+        for(i=l;i<=r;i++) input[i] = b[i];
     }
 }
 
