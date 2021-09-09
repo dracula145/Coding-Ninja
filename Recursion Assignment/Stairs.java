712bcfe1541a98981456498f518dbf6c7e7ffa3c
Gajraj Mohini <mohinigajraj145@gmail.com>
2021-09-09 07:02:48 +0530

    final recursion assignment

--- Recursion Assignment/Stairs.java
+++ Recursion Assignment/Stairs.java
@@ -7,10 +7,10 @@ public class Stairs {
 		System.out.println(staircase(n));
 	}
     public static int staircase(int n){
-		if(n<=0) return 0;
+		if(n==0) return 1;
 		if(n==1) return 1;
 		if(n==2) return 2;
-		if(n==3) return 4;
+		//if(n==3) return 4;  // i had used this and in n==0 returned 0
 		return staircase(n-1) + staircase(n-2) + staircase(n-3);
     }
 }
