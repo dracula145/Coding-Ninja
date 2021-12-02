daab0cd904088508fd6a1a154513bbcdd62df7da
Gajraj Mohini <mohinigajraj145@gmail.com>
2021-12-02 12:16:48 +0530

    done

--- OOPS -4/TicTacToe/Board.java
+++ OOPS -4/TicTacToe/Board.java
@@ -21,6 +21,7 @@ public class Board {
         }
     }
 
+    // adds moves to the board and checks after each addition the status of game
     public int move(char symbol, int x, int y){
         if(x>=size || y>=size || board[x][y]!=' ') return INVALID;
         board[x][y] = symbol; // this.board not necessary because only one board in one game
