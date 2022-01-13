f92ae38a2245b18d7af1db5d9d2059871420cc53
Gajraj Mohini <mohinigajraj145@gmail.com>
2022-01-13 20:40:57 +0530

    new change

--- Hash Map/Assignment/PairsWithDiffK.java
+++ Hash Map/Assignment/PairsWithDiffK.java
@@ -27,19 +27,24 @@ import java.util.HashMap;
 public class PairsWithDiffK {
     public static int getPairsWithDifferenceK(int arr[], int k) {
         int result = 0;
-		HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i : arr) { // only iterate once... best approach
-            if(!map.containsKey(i)) map.put(i, 1);
-            else map.put(i, map.get(i)+1);
-               int t1 = Math.abs(i-k), t2 = i+k;
-            if(map.containsKey(t1) && k!=0) result += map.get(t1);  //watch hint video last approach
-            if(map.containsKey(t2) && k!=0) result += map.get(t2);  //watch hint video last approach
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) { // only iterate once... best approach
+            if (!map.containsKey(i))
+                map.put(i, 1);
+            else
+                map.put(i, map.get(i) + 1);
+            int t1 = i - k, t2 = i + k;
+            if (map.containsKey(t1) && k != 0)
+                result += map.get(t1); // watch hint video last approach
+            if (map.containsKey(t2) && k != 0)
+                result += map.get(t2); // watch hint video last approach
         }
         int res2 = 0;
-        if(k==0){
-         for(Integer i : map.keySet())  res2 += map.get(i)*(map.get(i)-1)/2;     // nC2 = n(n-1)/2
+        if (k == 0) {
+            for (Integer i : map.keySet())
+                res2 += map.get(i) * (map.get(i) - 1) / 2; // nC2 = n(n-1)/2
             return res2;
         }
         return result;
-	}
+    }
 }
