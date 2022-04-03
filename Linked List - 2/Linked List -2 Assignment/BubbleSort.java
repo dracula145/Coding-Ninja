3ad974192ec3c4c28781d232c1baaff30d8115e5
Gajraj Mohini <mohinigajraj145@gmail.com>
2022-04-03 21:55:46 +0530

    new change

--- Linked List - 2/Linked List -2 Assignment/BubbleSort.java
+++ Linked List - 2/Linked List -2 Assignment/BubbleSort.java
@@ -37,3 +37,49 @@ public class BubbleSort {
         b.data = temp;
     }
 }
+
+
+/*
+
+public class Solution {
+    public static int LengthLL(LinkedListNode<Integer> head) {
+		int count=1;
+		while(head.next!=null) {
+			head=head.next;
+			count++;
+		}
+		return count;
+	}
+
+	public static LinkedListNode<Integer> bubbleSort(LinkedListNode<Integer> head) {
+		//Your code goes here
+        	if(head == null || head.next == null) {
+			return head;
+		}
+		for(int i=0;i<LengthLL(head)-1;i++) {
+			LinkedListNode<Integer> prev=null,curr=head,next=curr.next;
+			while(curr.next!=null) {
+				if(curr.data>curr.next.data) {
+					if(prev == null) {
+						curr.next=next.next;
+						next.next=curr;
+						prev=next;
+						head=prev;
+					}else {
+						next=curr.next;
+						curr.next=next.next;
+						prev.next=next;
+						next.next=curr;
+						prev=next;
+					
+					}
+				}else {
+					prev=curr;
+					curr=curr.next;
+				}
+			}
+		}
+		return head;
+	}
+}
+*/
