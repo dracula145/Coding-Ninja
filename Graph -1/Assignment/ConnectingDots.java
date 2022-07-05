84c825876db2900f418078727f521b4373e3c8ac
Gajraj Mohini <mohinigajraj145@gmail.com>
2022-07-05 11:32:56 +0530

    5july

--- Graph -1/Assignment/ConnectingDots.java
+++ Graph -1/Assignment/ConnectingDots.java
@@ -29,83 +29,35 @@ true
 */
 
 import java.util.*;
-
 public class ConnectingDots {
+    
+    private int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    
+    private boolean dfs(String[] board, int i, int j, int pi, int pj, int n, int m, boolean[][] visited){
+        visited[i][j] = true;
+        char curr = board[i].charAt(j);
+        for(int[] d : dir){
+            int new_i = i + d[0];
+            int new_j = j + d[1];
+            if(isSafe(new_i, new_j, n, m)              // safe boundary
+                && (new_i != pi || new_j != pj)           // next cell not parent
+                && board[new_i].charAt(new_j) == curr)           // next cell must be of same character
+                if(visited[new_i][new_j] || dfs(board, new_i, new_j, i, j, n, m, visited)) return true;
+        }
+        return false;    
+    }
 
-    private void initialize(boolean[][] visited, int n, int m){
-	for (int i = 0;i < n;i++)
-	{
-		for (int j = 0;j < m;j++) visited[i][j] = false;
-	}
-}
-
-private boolean dfs(String[] board, char col, int pi, int pj, int i, int j, int n, int m, boolean[][] visited)
-{
-
-
-	boolean d1 = i + 1 < n && !(i + 1 == pi && j == pj) && visited[i + 1][j];
-	boolean d2 = j + 1 < m && !(i == pi && j + 1 == pj) && visited[i][j + 1];
-	boolean d3 = i - 1 >= 0 && !(i - 1 == pi && j == pj) && visited[i - 1][j];
-	boolean d4 = j - 1 >= 0 && !(i == pi && j - 1 == pj) && visited[i][j - 1];
-
-	if (d1 || d2 || d3 || d4)
-	{
-		return true;
-	}
-
-	boolean ans = false;
-	visited[i][j] = true;
-	if (i + 1 < n && board[i + 1].charAt(j) == col && !visited[i + 1][j])
-	{
-		ans = ans || dfs(board, col, i, j, i + 1, j, n, m, visited);
-	}
-
-	if (j + 1 < m && board[i].charAt(j+1) == col && !visited[i][j + 1])
-	{
-		ans = ans || dfs(board, col, i, j, i, j + 1, n, m, visited);
-	}
-
-	if (i - 1 >= 0 && board[i - 1].charAt(j) == col && !visited[i - 1][j])
-	{
-		ans = ans || dfs(board, col, i, j, i - 1, j, n, m, visited);
-	}
-
-	if (j - 1 >= 0 && board[i].charAt(j-1) == col && !visited[i][j - 1])
-	{
-		ans = ans || dfs(board, col, i, j, i, j - 1, n, m, visited);
-	}
-
-	return ans;
-}
-
-public int solve(String[] board, int n, int m)
-{
-	
-	boolean[][] visited = new boolean[n][m];
-	
-	initialize(visited, n, m);
-	for (int i = 0;i < n;i++)
-	{
-		for (int j = 0;j < m;j++)
-		{
-			boolean ans = false;
-			if (!visited[i][j])
-			{
-				char color = board[i].charAt(j);
-				ans = dfs(board, color, i, j, i, j, n, m, visited);
-				initialize(visited, n, m);
-				//cout<<"======"<<endl;
-				//cout<<endl;
-				//cout<<ans<<endl;
-				if (ans)
-				{
-					return 1;
-				}
-			}
-		}
-	}
-	return 0;
-}
-	
-	
+    private boolean isSafe(int i, int j, int n, int m){
+        return i>=0 && i<n && j>=0 && j<m;
+    }
+    
+    public int solve(String[] board, int n, int m)
+    {
+        boolean[][] visited = new boolean[n][m];
+        for (int i = 0;i < n;i++){
+            for (int j = 0;j < m;j++)
+                if(!visited[i][j] && dfs(board, i, j, -1, -1, n, m, visited)) return 1;
+        }
+        return 0;
+    }
 }
